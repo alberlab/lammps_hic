594630206a04c1bd71eb055d8ab598b89d9d3fd8
Guido Polles <polles@usc.edu>
2017-01-23 15:58:45 -0800

    block strategy for ACTDIST

--- lammps_hic/parallel.py
+++ lammps_hic/parallel.py
@@ -2,8 +2,13 @@ import h5py
 import logging
 import numpy as np
 
-from . import myio as io
 
+def pretty_tdelta(seconds):
+    seconds = int(seconds)
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%dh %02dm %02ds" % (h, m, s)
+        
 
 def monitor_progress(routine, async_results, timeout=60):
     while not async_results.ready():
@@ -11,11 +16,11 @@ def monitor_progress(routine, async_results, timeout=60):
                      routine,
                      async_results.progress,
                      len(async_results),
-                     async_results.elapsed)
+                     pretty_tdelta(async_results.elapsed))
         async_results.wait(timeout)
 
 
-def _compute_actdist(crd_fname, nstruct, nbead):
+def _compute_actdist(nstruct, nbead):
     '''Calculate actdist closure. Returns a function,
     taking as arguments i, j, pwish, plast.
     Using a closure makes sure that even using
@@ -24,8 +29,6 @@ def _compute_actdist(crd_fname, nstruct, nbead):
     dictionaries. Hopefully this minimizes unneeded
     communication. However, a direct view is probably
     better here.'''
-    crd = {}
-    radius = {}
     
     def existingPortion(v, rsum):
         return sum(v<=rsum)*1.0/len(v)
@@ -36,98 +39,119 @@ def _compute_actdist(crd_fname, nstruct, nbead):
         else:
             pclean = pij
         return max(0, pclean)
-    
-    def get_crd(i):
-        try:
-            return crd[i], crd[i+nbead], radius[i]
-        except KeyError:
-            hss = h5py.File(crd_fname, 'r')
-            crd[i] = hss['coordinates'][:,i,:][()]
-            crd[i + nbead] = hss['coordinates'][:,i + nbead,:][()]
-            radius[i] = hss['radius'][i][()]
-        return crd[i], crd[i+nbead], radius[i]
         
-    def inner(i, j, pwish, plast):
+    def inner(data):
         '''The function to be returned'''
         import numpy as np
-
-        x1, x2, ri = get_crd(i)
-        y1, y2, rj = get_crd(j)
+        results = []
+        local_i, local_j, radii, to_process = data
         
-        d_sq = np.empty( (4, nstruct) )
-        d_sq[0] = np.sum(np.square(x1 - y1), axis=1)
-        d_sq[1] = np.sum(np.square(x2 - y2), axis=1)
-        d_sq[2] = np.sum(np.square(x1 - y2), axis=1)
-        d_sq[3] = np.sum(np.square(x2 - y1), axis=1)
+        for i, j, pwish, plast in to_process:
+            
+            x1 = local_i['x1'][i-local_i['offset1']] 
+            x2 = local_i['x2'][i-local_i['offset2']]
+            y1 = local_j['x1'][j-local_j['offset1']] 
+            y2 = local_j['x2'][j-local_j['offset2']]
+            
+            
+            ri, rj = radii[i], radii[j]
 
-        sel_dist = np.empty(nstruct*2)
-        pnow = 0
-        rcutsq = np.square( 2*(ri+rj) )
+            d_sq = np.empty( (4, nstruct) )
+            d_sq[0] = np.sum(np.square(x1 - y1), axis=1)
+            d_sq[1] = np.sum(np.square(x2 - y2), axis=1)
+            d_sq[2] = np.sum(np.square(x1 - y2), axis=1)
+            d_sq[3] = np.sum(np.square(x2 - y1), axis=1)
 
-        for n in range(nstruct):
-            z = d_sq[:, n]
+            sel_dist = np.empty(nstruct*2)
+            pnow = 0
+            rcutsq = np.square( 2*(ri+rj) )
 
-            # assume the closest pair as the first (eventual) contact  
-            # and keep the distance of the only other pair which is compatible with it.
-            m = np.argsort(z)[0]
-            if m == 0 or m == 1:
-                sel_dist[2*n:2*n+2] = z[0:2]
-                pnow += np.sum( z[0:2] <= rcutsq )    
-            else:                   
-                sel_dist[2*n:2*n+2] = z[2:4]
-                pnow += np.sum( z[2:4] <= rcutsq )    
+            for n in range(nstruct):
+                z = d_sq[:, n]
 
-        pnow = float(pnow) / (2.0 * nstruct)
-        sortdist_sq = np.sort(sel_dist)
+                # assume the closest pair as the first (eventual) contact  
+                # and keep the distance of the only other pair which is compatible with it.
+                m = np.argsort(z)[0]
+                if m == 0 or m == 1:
+                    sel_dist[2*n:2*n+2] = z[0:2]
+                    pnow += np.sum( z[0:2] <= rcutsq )    
+                else:                   
+                    sel_dist[2*n:2*n+2] = z[2:4]
+                    pnow += np.sum( z[2:4] <= rcutsq )    
 
-        t = cleanProbability(pnow, plast)
-        p = cleanProbability(pwish,t)
+            pnow = float(pnow) / (2.0 * nstruct)
+            sortdist_sq = np.sort(sel_dist)
 
-        res = None
-        if p>0:
-            o = min(2 * nstruct - 1, int(round(2 * p * nstruct)))
-            activation_distance = np.sqrt(sortdist_sq[o]) - (ri + rj)
-            res = (activation_distance, p, pnow)
+            t = cleanProbability(pnow, plast)
+            p = cleanProbability(pwish,t)
 
-        return res
+            res = None
+            if p>0:
+                o = min(2 * nstruct - 1, int(round(2 * p * nstruct)))
+                activation_distance = np.sqrt(sortdist_sq[o]) - (ri + rj)
+                res = (i, j, pwish, activation_distance, p, pnow)
+                results.append(res)
+        return results
+    
     return inner
 
 
+
 def get_actdists(parallel_client, crd_fname, probability_matrix, theta, last_ad, save_to=None):
+    
+    hss = h5py.File(crd_fname, 'r')
+    n_struct = hss['nstruct'][()]
+    radii = hss['radius'][()]
+    crd = hss['coordinates'][()]
+    
+    n_bead = len(probability_matrix)
 
-    with h5py.File(crd_fname, 'r') as hss:
-        n_struct = hss['nstruct'][()]
-        n_bead = hss['nbead'][()]
-
-    to_process = []
-
-    last_prob = {(i, j): d for i, j, d in last_ad}
-
+    last_prob = {(i, j): p for i, j, pw, d, p, pn in last_ad}
+    
+    n_workers = len(parallel_client.ids)
+                
+    if n_workers == 0:
+        logging.error('get_actdists(): No Engines Registered')
+        raise RuntimeError('get_actdists(): No Engines Registered')
+    
+    blocks_per_line = 2 * int(np.sqrt(0.25 + 2 * n_workers) - 0.5)
+    block_size = (n_bead // blocks_per_line) + 1
+    blocks = {(i, j): list() for i in range(blocks_per_line) for j in range(i, blocks_per_line)}
+    
     for i in range(n_bead):
         for j in range(i + 2, n_bead): # skips consecutive beads
             if probability_matrix[i, j] >= theta:
                 try:
                     lp = last_prob[(i, j)]
                 except KeyError:
-                    lp = 0
-                to_process.append((i, j, probability_matrix[i, j], lp))
-
-    dview = parallel_client[:]
-    async_results = dview.apply(_compute_actdist(crd_fname,
-                                                 n_struct,
-                                                 n_bead), # using the closure
-                                to_process)
-    
-    monitor_progress('get_actdists', async_results)
+                    lp = 0    
+                blocks[(i // block_size, j // block_size)].append((i, j, probability_matrix[i, j], lp))
         
-    results = async_results.get()
-
+    local_data = []
+    for k in range(blocks_per_line):
+        offset1 = k * block_size
+        offset2 = k * block_size
+        x1 = crd[:, k * block_size:min((k + 1) * block_size, n_bead), :].transpose((1, 0, 2))
+        x2 = crd[:, k * block_size + n_bead:min((k + 1) * block_size + n_bead, 2*n_bead), :].transpose((1, 0, 2))
+        local_data.append({'offset1' : offset1, 'offset2': offset2, 'x1': x1, 'x2': x2})
+    
+    args = []
+    for bi in range(blocks_per_line):
+        for bj in range(bi, blocks_per_line):
+            args.append((local_data[bi], local_data[bj], radii, blocks[(bi, bj)]))
+    
+    lbview = parallel_client.load_balanced_view()
+    async_results = lbview.map_async(_compute_actdist(n_struct, n_bead), args)  # using the closure
+    
+    monitor_progress('get_actdists()', async_results)
+        
+    results = []
+    for r in async_results.get():
+        results += r
+        
     if save_to is not None:
         np.savetxt(save_to,
-                   [i for i in zip(to_process, results)],
-                   fmt=io.ACTDIST_TXT_FMT)
+                   results,
+                   fmt='%6d %6d %.5f %10.2f %.5f %.5f')  #myio.ACTDIST_TXT_FMT)
 
-    ad = []
-    for i, j, pwish, d, pclean, pnow in zip(to_process, results):
-        ad.append(i, j, d)
-    return ad
+    return results, args
